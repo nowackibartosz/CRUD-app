b8a5f61bbaccad062388c222c782e595b67bb77f
Bartosz Nowacki <nowackib@protonmail.com>
2022-10-17 22:40:09 +0200

    .

--- src/App.js
+++ src/App.js
@@ -13,6 +13,24 @@ import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+
+
+// # Zadanie 1
+
+// Stwórz routing aplikacji (bazując na poprzednich zadaniach). Potrzebne będą:
+// * /clients -> podstrona z danymi kontaktowymi klientów
+// * /clients/add -> podstrona na dodawanie klientów
+// * /clients/:id -> podstrona na detale klienta,
+// * /clients/:id/edit -> podstrona na edycję klienta,
+// * /orders - > podstrona na wszystkie zamowienia,
+// * /orders/:id -> podstrona na podgląd pojedynczego zamowienia,
+// * /orders/add -> podstrona na dodawanie zamowien,
+// * /invoices - > podstrona na faktury,
+
+
+
+
+
 function App() {
   return (
     <div className="App">
