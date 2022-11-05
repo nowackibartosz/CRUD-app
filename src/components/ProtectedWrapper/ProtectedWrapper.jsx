65c64502e8ea78ceb3b4811265710905b2fc20c0
Bartosz Nowacki <nowackib@protonmail.com>
2022-11-05 21:21:05 +0100

    .

--- src/components/ProtectedWrapper/ProtectedWrapper.jsx
+++ src/components/ProtectedWrapper/ProtectedWrapper.jsx
@@ -4,7 +4,12 @@ import { useUserContext } from "../UserContext/UserContext";
 const ProtectedWrapper = ({ children }) => {
   const { isLogged } = useUserContext();
 
-  return <div className="wrapik">{isLogged ? { children } : null} </div>;
+    return(
+    
+        <div>{children}</div>
+        // <div className="wrapik">{isLogged ? { children } : null} </div>
+    
+    )
 };
 
 export default ProtectedWrapper;
