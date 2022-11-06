6d69690fd8a62cc683f7608d6b6e8cd7442444fa
Bartosz Nowacki <nowackib@protonmail.com>
2022-11-06 19:25:06 +0100

    QueryClient.invalidateQueries

--- src/components/Zad1/Clients/Clients.jsx
+++ src/components/Zad1/Clients/Clients.jsx
@@ -1,11 +1,12 @@
 import "./Clients.css";
 import { Link } from "react-router-dom";
-import { useQuery, QueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getAllClients, handleDelete } from "../../Serwis/clientService";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
+import { useState } from "react";
 
 const style = {
   position: "absolute",
@@ -24,7 +25,12 @@ const style = {
 const Clients = () => {
   const { data, isLoading } = useQuery(["clients"], getAllClients);
 
-  const [open, setOpen] = React.useState(false);
+
+  const queryClient = useQueryClient()
+
+  const [open, setOpen] = useState(false);
+  const [elModal, setElModal] = useState("");
+
   const handleOpen = () => {
     setOpen(true);
   };
@@ -64,39 +70,42 @@ const Clients = () => {
                 <Button
                   onClick={() => {
                     handleOpen();
+                    setElModal(el.id);
                     console.log(el.id);
                   }}
                 >
                   DELETE
                 </Button>
-                <Modal
-                  open={open}
-                  onClose={handleClose}
-                  aria-labelledby="parent-modal-title"
-                  aria-describedby="parent-modal-description"
-                >
-                  <Box sx={{ ...style, width: 400 }}>
-                    <p className="parent-modal-description">Are you sure?</p>
-
-                    <div className="hihi">
-                      <Button
-                        onClick={() => {
-                          handleDelete(el.id);
-                          console.log(el.id);
-                          // QueryClient.invalidateQueries({ queryKey: ["clients"] })
-                        }}
-                      >
-                        YES
-                      </Button>
-                      <Button onClick={handleClose}>NO</Button>
-                    </div>
-                  </Box>
-                </Modal>
               </th>
             </tr>
           ))}
         </tbody>
       </table>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <p className="parent-modal-description">Are you sure?</p>
+
+          <div className="hihi">
+            <Button
+              onClick={() => {
+                handleDelete(elModal);
+                console.log(elModal);
+                queryClient.invalidateQueries({ queryKey: ['clients'] })
+                handleClose();
+                
+              }}
+            >
+              YES
+            </Button>
+            <Button onClick={handleClose}>NO</Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 };
