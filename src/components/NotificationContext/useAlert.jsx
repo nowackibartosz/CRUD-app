31b0e515f7205a15462cfe104a363e877a356ede
Bartosz Nowacki <nowackib@protonmail.com>
2022-11-07 22:25:19 +0100

    .

--- src/components/NotificationContext/useAlert.jsx
+++ src/components/NotificationContext/useAlert.jsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
+import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 
-export const useAlert =() => {
-  const [isOpen, setIsOpen] = React.useState(false);
+export const useAlert = () => {
+    const history = useHistory()
+    const [isOpen, setIsOpen] = React.useState(false);
     const [message, setMessage] = React.useState("");
-    
+    const [route, setRoute] = React.useState("");
+    const [actionLabel, setActionLabel] = React.useState("");
 
-  const openAlert = ({message}) => {
+  const openAlert = ({message, route, actionLabel }) => {
       setIsOpen(true);
-      setMessage(message)
+      setMessage(message);
+      setRoute(route);
+      setActionLabel(actionLabel)
   };
 
   const closeAlert = (event, reason) => {
@@ -24,9 +29,9 @@ export const useAlert =() => {
 
   const action = (
     <React.Fragment>
-      <Button color="secondary" size="small" onClick={closeAlert}>
-        UNDO
-      </Button>
+          {route && <Button color="secondary" size="small" onClick={() => history.push(route)}>
+              {actionLabel}
+          </Button>}
       <IconButton
         size="small"
         aria-label="close"
