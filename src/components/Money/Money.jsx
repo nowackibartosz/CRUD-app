9315876c37cefb358c6d0c745c36b82f06c3af9a
Bartosz Nowacki <nowackib@protonmail.com>
2022-11-23 20:31:29 +0100

    redux ciag dalszy

--- src/components/Money/Money.jsx
+++ src/components/Money/Money.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { withdrawMoney,depositMoney } from "../../redux/moneySlice"
+import { withdrawMoney, depositMoney } from "../../redux/moneySlice";
 
 const Money = () => {
   const dispatch = useDispatch();
   const money = useSelector((state) => state.money.value);
 
-
-
   return (
     <>
-    <button onClick={() => dispatch(withdrawMoney())}>-</button>
-    {money}
-    <button onClick={() => dispatch(depositMoney())}>+</button>
+      <button onClick={() => dispatch(withdrawMoney())}>-</button>
+      {money}
+
+      <button onClick={() => dispatch(depositMoney())}>+</button>
     </>
-  )
+  );
 };
 
 export default Money;
